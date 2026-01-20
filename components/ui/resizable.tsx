0d41ac75e7d65f44c307b4acbc4b108dcdf374e7
ravindrauiet <ravindranathjha76@gmail.com>
2026-01-20 11:30:19 +0530

    feat: add `ResizablePanelGroup`, `ResizablePanel`, and `ResizableHandle` components for resizable UI layouts.

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -1,16 +1,16 @@
 "use client"
 
 import { GripVertical } from "lucide-react"
-import * as ResizablePrimitive from "react-resizable-panels"
+import { Group, Panel, Separator } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 function ResizablePanelGroup({
   className,
   ...props
-}: React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelGroup>) {
+}: React.ComponentPropsWithoutRef<typeof Group>) {
   return (
-    <ResizablePrimitive.PanelGroup
+    <Group
       className={cn(
         "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
         className
@@ -20,17 +20,17 @@ function ResizablePanelGroup({
   )
 }
 
-const ResizablePanel = ResizablePrimitive.Panel
+const ResizablePanel = Panel
 
 function ResizableHandle({
   withHandle,
   className,
   ...props
-}: React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelResizeHandle> & {
+}: React.ComponentPropsWithoutRef<typeof Separator> & {
   withHandle?: boolean
 }) {
   return (
-    <ResizablePrimitive.PanelResizeHandle
+    <Separator
       className={cn(
         "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
         className
@@ -42,7 +42,7 @@ function ResizableHandle({
           <GripVertical className="h-2.5 w-2.5" />
         </div>
       )}
-    </ResizablePrimitive.PanelResizeHandle>
+    </Separator>
   )
 }
 
